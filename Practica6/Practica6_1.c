e9b1c445189f3e7242eb1df2758db8e407534b49
Gonzalo93 <goncho_hdez@hotmail.com>
2016-10-04 18:21:33 +0200

    ejercicio 1,2

--- Practica6/Practica6_1.c
+++ Practica6/Practica6_1.c
@@ -1,1 +1,56 @@
-n
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int primo(int n);
+
+void main(int argc, char* argv[]){
+
+	int numero;
+	int i;
+	int k;
+	int numeros[100];
+	int vec_primos[100];
+	time_t t;
+
+	srand((unsigned) time(&t));
+	do{
+		printf("Introduce un numero.\n");
+		scanf("%d",&numero);
+		if(numero < 0 || numero > 100)
+			printf("Error.\n");
+
+	}while(numero < 0 || numero > 100)
+
+	//Producir numeros aleatorios
+	for(i = 0; i<numero; i++){
+		numeros[i] = rand();
+	}
+	
+	//Guardar los numeros primos en el vector vec_primos
+	for(i = 0, k=0; i<numero; i++){
+		if(primo(numeros[i] == 1){
+			vec_primos[k] = numeros[i];
+			k++;
+		}
+	}
+	
+	// imprimir todos los numeros primos
+	for(i=0; i < k; i++){
+		printf("%d ",vec_primos[k]);
+	}
+
+}
+
+int primo(int n){
+	int i;
+	int es_primo = 1;
+
+	for(i=2;i<n && es_primo != 0;i++){
+		if(n % i == 0){
+			es_primo = 0;
+		}		
+
+	}
+	return es_primo;
+}
---
+++ Practica6/Practica6_2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define TAM 50
+
+void main(int argc, char* argv[]){
+
+	char cadena[TAM];
+	char nueva[TAM];
+	int i;
+	
+	gets(cadena);
+	
+	for(i = 0 ; i < TAM ; i++){
+		if(cadena[i] >= '0' && cadena[i] <= '9'){
+			nueva[i] = '&';
+		}else if(cadena[i] >= 'a' && cadena[i] <= 'z'){ //Si es minuscula pasar a mayuscula
+			nueva[i] = cadena[i] - ('a' - 'A');
+		}else if(cadena[i] >= 'A' && cadena[i] <= 'Z'){
+			nueva[i] = cadena[i] + ('a' - 'A');
+		}	
+	}
+	printf("Original: %s\n  Nueva: %s \n",cadena,nueva);
+}

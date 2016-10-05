72d44724189c43ee5b79d8cb58b494c37430853f
Gonzalo93 <goncho_hdez@hotmail.com>
2016-10-05 16:24:31 +0200

    Pr6 ej3

--- Practica6/Practica6_3.c
+++ Practica6/Practica6_3.c
@@ -2,7 +2,46 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define TAM 100
 
+void Leer(double vec[],int n);
+void Mostrar(int array[],int n);
 void main(int argc, char* argv[]){
-
+	double vector1[TAM];
+	double vector2[TAM];
+	double vector_suma[TAM];
+	int tamano;
+	do{
+		printf("Que dimension de vector quieres usar?\n");
+		scanf("%d",&tamano);
+		if(tamano > TAM)
+			printf("Error, dimension no valida.\n");
+	}while(tamano > TAM);
+	
+	Leer(vector1,tamano);	
+	Mostrar(vector1,tamano);
+	
+	
 }
+
+void Leer(double vec[],int n){
+
+	int i;
+	printf("Introduce los valores del vector: \n");
+	for(i=0;i<n;i++){
+		printf("valor %d: \n",i);
+		scanf("",&vec[i]);
+	}
+	return;
+}
+
+	
+void Mostrar(int array[],int n){
+	int i;
+	for(i=0; i< n; i++){
+		printf("%d ",array[i]);
+	}
+	printf("\n");
+	return;
+}	
+	

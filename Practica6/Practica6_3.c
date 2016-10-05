965a91014da3127b8cd966a2edf9a39adc70280e
Gonzalo93 <goncho_hdez@hotmail.com>
2016-10-05 16:43:14 +0200

    acabado P6 ej3

--- Practica6/Practica6_3.c
+++ Practica6/Practica6_3.c
@@ -6,12 +6,17 @@
 
 void Leer(double vec[],int n);
 void Mostrar(double array[],int n);
+double ProductoEscalar(double vec1[],double vec2[],int n);
+void Multiplicar(double vec[],double escalar,int n);
+void SumarVectores(double vec1[], double vec2[], double vec_suma[], int n);
 
 void main(int argc, char* argv[]){
 	double vector1[TAM];
 	double vector2[TAM];
 	double vector_suma[TAM];
 	int tamano;
+	double resultadoEscalar,x;
+	
 	do{
 		printf("Que dimension de vector quieres usar?\n");
 		scanf("%d",&tamano);
@@ -20,10 +25,56 @@ void main(int argc, char* argv[]){
 	}while(tamano > TAM);
 	
 	Leer(vector1,tamano);	
+	Leer(vector2,tamano);	
+	Mostrar(vector1,tamano);
+	Mostrar(vector2,tamano);
+	
+	resultadoEscalar=ProductoEscalar(vector1,vector2,tamano);
+	
+	printf("Producto Escalar: %lf\n",resultadoEscalar);
+	printf("Valor de X: \n");
+	scanf("%lf",&x);
+	
+	Multiplicar(vector1,x,tamano);
+	printf("Multiplicacion:\n");
 	Mostrar(vector1,tamano);
 	
+	SumarVectores(vector1, vector1, vector_suma, tamano);
+	printf("Suma:\n");
+	Mostrar(vector_suma,tamano);
 	
 }
+void SumarVectores(double vec1[], double vec2[], double vec_suma[], int n){
+	int i;
+	
+	for(i=0;i<n;i++){
+		vec_suma[i]=vec1[i] +vec2[i];
+	}
+	return;
+}
+
+
+void Multiplicar(double vec[],double escalar,int n){
+	int i;
+	
+	for(i=0;i<n;i++){
+		vec[i] = vec[i] * escalar;
+	}
+	return;	
+}
+
+
+
+double ProductoEscalar(double vec1[],double vec2[],int n){
+	int i;
+	double pe=0;
+	
+	for(i=0;i<n;i++){
+		pe= pe + vec1[i] * vec2[i] ;
+	}
+	return pe;
+}
+
 
 void Leer(double vec[],int n){
 
@@ -39,6 +90,7 @@ void Leer(double vec[],int n){
 	
 void Mostrar(double array[],int n){
 	int i;
+	printf("Vector: \n");
 	for(i=0; i< n; i++){
 		printf("%lf ",array[i]);
 	}

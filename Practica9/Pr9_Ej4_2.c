359c94727bd067a96906380af29197fb639ff0f1
Gonzalo93 <goncho_hdez@hotmail.com>
2016-11-07 19:34:02 +0100

    matrices

--- Practica9/Pr9_Ej4_2.c
+++ Practica9/Pr9_Ej4_2.c
@@ -11,20 +11,47 @@ void main(int argc, char* argv[]){
 	int matriz2[M][M];
 	int resultado[M][M];
 	
+	int i,j;
 	//Pedir matrices
-	
+	printf("Introduce matriz 1\n");
+	for(i=0;i<M;i++){
+		for(j=0;j<M;j++){
+			printf("Elemento: ");
+			scanf("%d",&matriz1[i][j]);
+		}
+	}
+	printf("Introduce matriz 2\n");
+	for(i=0;i<M;i++){
+		for(j=0;j<M;j++){
+			printf("Elemento: ");
+			scanf("%d",&matriz2[i][j]);
+		}
+	}
 	//Llamada funcion
+	ProdMat(matriz1,matriz2,int **resultado);
+	printf("Matriz Producto:\n");
+	for(i=0;i<M;i++){
+		printf("\n");
+		for(j=0;j<M;j++){
+			printf("%d  ",resultado[i][j]);			
+		}
+	}
 	
 }
 
-void ProdMat(int **matriz1,int **matriz2,int **resultado){
+void ProdMat(int matriz1[][M],int matriz2[][M],int resultado[][M]){
 	int i,j,k;
-	
-	for(i=0; i<M; ++i)
-		for(j=0; j<M; ++j)
+	int suma;
+	for(i=0; i<M; ++i){
+		for(j=0; j<M; ++j){
+			suma=0;
 			for(k=0; k<M; ++k){
-                resultado[i][j]+=matriz1[i][k]*matriz2[k][j];
+                suma= suma + matriz1[i][k]*matriz2[k][j];
             }
+			resultado[i][j] = suma;
+		}
+	}
+	return;		
 	
 }
 
